69a8ae3b687fa3389b41a09404fc2c957d9cfe67
Student <foobar@sr.htlweiz.at>
2025-12-01 11:20:04 +0100

    1.12.2025_11:19

---
+++ Klassen/Library.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleBinaryConverter;
+
+using System;
+
+public class DecimalConv{
+    public static Int64 Bi_to_Decimal(bool[] bi_numb, int bi_lenght){
+        
+        Int64 deci_numb = 0;
+
+        for(int count = 0; count < bi_lenght; count ++){
+            
+            if(bi_numb[count] == true){
+                deci_numb += Convert.ToInt64(Math.Pow(2, bi_lenght-(count+1)));
+            }
+        }
+
+        return deci_numb;
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleBinaryConverter;
+
+using System;
+using SimpleBinaryConverter;
+
+public class DecimalConv{
+    public static Int64 Bi_to_Decimal(bool[] bi_numb, int bi_lenght){
+        
+        Int64 deci_numb = 0;
+
+        for(int count = 0; count < bi_lenght; count ++){
+            
+            if(bi_numb[count] == true){
+                deci_numb += Convert.ToInt64(Math.Pow(2, bi_lenght-(count+1)));
+            }
+        }
+
+        return deci_numb;
+    }
+}
+
+public class Program{
+    public static void Main(){
+        bool[] bi_numb = {true, false, false, false};
+        
+       Console.WriteLine(DecimalConv.Bi_to_Decimal(bi_numb, bi_numb.Length));
+
+    }
+}
